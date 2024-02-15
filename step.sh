52754705acc199a1e7ddb7109e1443d972475f5c
zenled <zen.lednik@infinum.com>
2024-02-15 10:25:19 +0100

    Extract FVM Flutter version

--- step.sh
+++ step.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 set -ex
 
-# Install FVM
-dart pub global activate fvm
-export PATH="$PATH":"$HOME/.pub-cache/bin"
-fvm install
-
-# Read fvm flutter version from project
-FVM_FLUTTER_VERSION="$(grep -o '"flutterSdkVersion": "[^"]*' .fvm/fvm_config.json | grep -o '[^"]*$')"
+FVM_FLUTTER_VERSION="$(grep -o '"flutter": "[^"]*' .fvmrc | grep -o '[^"]*$')"
 envman add --key FVM_FLUTTER_VERSION --value $FVM_FLUTTER_VERSION
-
-# Replace PATH to include fvms flutter first
-envman add --key PATH --value '/Users/vagrant/fvm/versions/$FVM_FLUTTER_VERSION/bin:$PATH'
-
-# Echo path to console
-envman run bash -c 'echo "Modified path: $PATH"'
